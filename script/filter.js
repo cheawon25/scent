effe4ca8a5f78399db85e502572cd810b20bf7d3
cheawon25 <99226711+cheawon25@users.noreply.github.com>
2023-11-21 17:35:38 +0900

    Add files via upload

---
+++ script/filter.js
@@ -0,0 +1,20 @@
+$(function(){
+  $('.prod_cen>ul>li').click(function () {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+  $('#all').click(function () {
+    $('.filter_content div').show();//서서히 나타나게 만들고 싶다면 .fadeIn()을 사용한다.
+  });
+  $('#filter1').click(function () {
+    $('.filter1').show();
+    $('.filter_content>div').not('.filter1').hide();//서서히 사라지게 만들고 싶다면 .fadeOut()을 사용한다.
+  });
+  $('#filter2').click(function () {
+    $('.filter2').show();
+    $('.filter_content>div').not('.filter2').hide();
+  });
+  $('#filter3').click(function () {
+    $('.filter3').show();
+    $('.filter_content>div').not('.filter3').hide();
+  });
+});
